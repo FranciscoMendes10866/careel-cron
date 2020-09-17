55d9896f84df675843cdcb31e3db405f4bd14827
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-09-17 17:29:48 +0100

    commented the code

--- src/server.ts
+++ src/server.ts
@@ -5,20 +5,25 @@ import moment from 'moment'
 // constants
 const CronJob = cron.CronJob
 
-// cron job function
+// cron job function (runs every 5 sec)
 const job = new CronJob('*/5 * * * * *', () => {
   // cron start date
   const jobDate = moment().format('LLLL')
   // logic
   console.log('-----------------------------------')
   console.log(`🍎 Cron job ${jobDate} executed. 🍏`)
+  // supposed data that comes from the database
   const sponsorDate = '2020-08-16T22:45:03.154Z'
-  const localTime = moment()
-  const otherTime = moment(sponsorDate)
-  const difference = (localTime.diff(otherTime, 'days') >= 24)
-  if (difference === true) {
+  // current execution timestamp
+  const currentTime = moment()
+  // converts the sponsorDate (string) into a timestamp
+  const convertSponsor = moment(sponsorDate)
+  // checks the difference between the two timestamps
+  const result = (currentTime.diff(convertSponsor, 'days') >= 24)
+  // response
+  if (result === true) {
     console.log('Yes')
-  } else if (difference === false) {
+  } else if (result === false) {
     console.log('No')
   } else {
     console.log('An error occored.')
